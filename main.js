1dc211fa153b406130d9f96d89d02cb3ea10bcf2
viktordepomian <viktor.depomian@gmail.com>
2022-07-12 19:27:47 +0200

    Added everything

---
+++ main.js
@@ -0,0 +1,6 @@
+function reverseString(str){
+    return str.split('').reverse().join('');
+};
+
+//Type in a string you want to be reversed. E.g. Tarzan -> nazraT
+console.log(reverseString('Gollum'));
